bdcf72d78074e70d6e7a7bba7e5cc6f871e67a68
Naga Vinay <nagavinay0905@gmail.com>
2025-12-09 14:33:50 +0530

    Organize DP solutions into Python and Java folders

--- minCostClimbingStairs.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-def minCostClimbingStairs(l:List[int]):
-    n=len(l)
-    dp=[0]*(n+1)
-    for i in range(2,n+1):
-        dp[i]=min(dp[i-1]+l[i-1],dp[i-2]+l[i-2])
-    print(dp[n])
-minCostClimbingStairs([10,15,20])
-minCostClimbingStairs([1,100,1,1,1,100,1,1,100,1])
