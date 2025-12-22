53bde99c900c66d299c31f4d5e96e9ad70613ab1
Naga Vinay <nagavinay0905@gmail.com>
2025-12-22 17:53:14 +0530

    new practice questions from praveen sir v2

--- python_solutions/practice_questions(praveen_sir)/Equilibrium_index.py
+++ python_solutions/practice_questions(praveen_sir)/Equilibrium_index.py
@@ -6,3 +6,26 @@
 # A[0] + A[1] + A[2] = A[4] + A[5] + A[6]
 # Input: A[] = {1, 2, 3}
 # Output: -1
+def Equilibrium_index(arr):
+    n=len(arr)
+    
+    sum_of_array=sum(arr)
+    
+    left_sum=0
+    
+    for i in range(n):
+        right_sum=sum_of_array-left_sum-arr[i]
+        if left_sum==right_sum:
+            return i
+        left_sum+=arr[i]
+            
+    return -1
+
+
+
+        
+A= [-7, 1, 5, 2, -4, 3, 0]
+
+B=[1, 2, 3]
+print(Equilibrium_index(A))
+print(Equilibrium_index(B))
