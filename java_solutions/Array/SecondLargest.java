40ca67d030c52e5f9d565edeee824e3ebd862176
Naga Vinay Avvaru <nagavinay0905@gmail.com>
2026-04-17 09:18:35 +0530

    Arrays revision

---
+++ java_solutions/Array/SecondLargest.java
@@ -0,0 +1,14 @@
+package java_solutions.Array;
+
+public class SecondLargest {
+    public static int SecondLargest(int[] arr){
+
+        
+        return 0;
+    }
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6,7,5,4,3,2,1};
+        int ans=SecondLargest(arr);
+        System.out.println(ans);
+    }
+}
---
+++ java_solutions/Array/largestNumber.java
@@ -0,0 +1,20 @@
+package java_solutions.Array;
+
+public class largestNumber {
+
+    public static int largestNumer(int[] arr){
+
+        int max=0;
+        
+        for(int i:arr){
+            max=Math.max(max, i);
+
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6,7,5,4,3,2,1};
+        int ans=largestNumer(arr);
+        System.out.println(ans);
+    }
+}
