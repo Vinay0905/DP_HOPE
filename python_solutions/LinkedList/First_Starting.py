ddabdf06332037c9c3ffc3c02e5345226947e736
Naga Vinay Avvaru <nagavinay0905@gmail.com>
2026-04-17 09:18:34 +0530

    New Commits for CSES

---
+++ python_solutions/LinkedList/First_Starting.py
@@ -0,0 +1,33 @@
+class Node():
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class LinkedList():
+    def __init__(self):
+            
+        self.head=None
+        
+    def push(self,new_data):
+        new_node=Node(new_data)
+        new_node.next=self.head
+        self.head=new_node
+    
+    def printList(self):
+        temp=self.head
+        while(temp):
+            print(temp.data,end=" ")
+            temp=temp.next
+            
+            
+                
+            
+if __name__=="__main__":
+    llist=LinkedList()
+    llist.push(20)
+    llist.push(4)
+    llist.push(15)
+    llist.push(10)
+    
+    print("Created Linked List is:")
+    llist.printList()
+        
