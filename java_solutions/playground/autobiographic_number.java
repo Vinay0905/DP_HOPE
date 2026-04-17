fa87c0943a115ec01fcdc4d3950d4e0869755488
Naga Vinay Avvaru <nagavinay0905@gmail.com>
2026-04-17 09:18:34 +0530

    Java traning

--- java_solutions/playground/autobiographic_number.java
+++ java_solutions/playground/autobiographic_number.java
@@ -6,7 +6,7 @@ public class autobiographic_number {
         int l=s.length();
 
         int[] freq=new int[l];
-        
+        return false;
     }
     public static void main(String[] args) {
         int n=1210;
