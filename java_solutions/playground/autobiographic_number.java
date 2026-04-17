81b46bef4bafa24a2c739876b73789cde86ef808
Naga Vinay Avvaru <nagavinay0905@gmail.com>
2026-04-17 09:18:35 +0530

    hashing and graph

--- java_solutions/playground/autobiographic_number.java
+++ java_solutions/playground/autobiographic_number.java
@@ -2,10 +2,10 @@ package java_solutions.playground;
 
 public class autobiographic_number {
     public static boolean Autobiograph(int n){
-        String s=String.valueOf(n);
-        int l=s.length();
+        //String s=String.valueOf(n);
+        //int l=s.length();
 
-        int[] freq=new int[l];
+        //int[] freq=new int[l];
         return false;
     }
     public static void main(String[] args) {
