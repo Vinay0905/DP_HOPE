8f4c75e18a908a7e0cc0c69d7f12691c80ac2104
Naga Vinay Avvaru <nagavinay0905@gmail.com>
2026-05-05 15:13:59 +0530

    Again here we go with arrays

--- java_solutions/Array/moveZerostoEnd.java
+++ java_solutions/Array/moveZerostoEnd.java
@@ -17,14 +17,32 @@ public class moveZerostoEnd {
                     arr[j]=t;
                     j++;
                 }
+        }
+        public static int[] bruteForce(int[] arr){
+            int n=arr.length;
+            int[] ne=new int[n];
+            int cnt=0;
+            
+            for(int i=0;i<n;i++){
+                if(arr[i]!=0){
+                    ne[cnt]=arr[i];
+                    cnt++;
+                }
+                
             }
+        return ne;
+        }
 
-    
+            
     public static void main(String[] args) {
         int[] nums={0,1,0,3,12};
         moveZero(nums);
+        // for(int i=0;i<nums.length;i++){
+        //     System.out.print(nums[i]+" ");
+        // }
+        int[] new1=bruteForce(nums);
         for(int i=0;i<nums.length;i++){
-            System.out.print(nums[i]+" ");
+            System.out.print(new1[i]+" ");
         }
     }
 }
