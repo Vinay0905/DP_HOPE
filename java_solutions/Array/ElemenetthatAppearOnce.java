8794a2622331d21fe284fefe5b76ecc227666056
Naga Vinay Avvaru <nagavinay0905@gmail.com>
2026-05-05 15:47:19 +0530

    Hashmap

--- java_solutions/Array/ElemenetthatAppearOnce.java
+++ java_solutions/Array/ElemenetthatAppearOnce.java
@@ -1,5 +1,8 @@
 package java_solutions.Array;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ElemenetthatAppearOnce {
     
     public static int bruteforce(int[] arr){
@@ -17,9 +20,42 @@ public class ElemenetthatAppearOnce {
         }
         return 0;
     }
+    public static int betterSolution(int[] arr){
+        int maxEle=0;
+        for(int i :arr){
+            maxEle=Math.max(i, maxEle);
+        }
+        int[] newarr=new int[maxEle+1];
+        for(int i=0;i<arr.length;i++){
+            newarr[arr[i]]++;
+            
+        }
+
+        for (int i = 0; i < newarr.length; i++) {
+        if (newarr[i] == 1) {
+            return i;
+        }
+    }
+        return 0;
+        
+
+    }
+    public static int optimalSolution(int[] arr){
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+        }
+
+        for(int i=0;i<arr.length;i++){
+            if(map.get(arr[i])==1){
+                return arr[i];
+            }
+        }
+        return 0;
+    }
     public static void main(String[] args) {
         int[] arr={1,1,2,3,3,4,4};
-        int ans=bruteforce(arr);
+        int ans=betterSolution(arr);
         System.out.println(ans);
     }
 }
