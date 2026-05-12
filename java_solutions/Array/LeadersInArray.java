7d0db226a80e91ab75f3eba404c7893d0b20e206
Naga Vinay Avvaru <nagavinay0905@gmail.com>
2026-05-12 21:35:48 +0530

    did 2 sums

---
+++ java_solutions/Array/LeadersInArray.java
@@ -0,0 +1,51 @@
+package java_solutions.Array;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LeadersInArray {
+    
+
+    public static List<Integer> bruteforce(int[] arr){
+        int n=arr.length;
+        List<Integer> ans=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            boolean leader=true;
+            for(int j=i+1;j<n;j++){
+                if(arr[i]<arr[j]){
+                    leader=false;
+                    break;
+                }
+            }
+
+            if(leader==true)ans.add(arr[i]);
+        }
+        return ans;
+
+    }
+    public static List<Integer> optimalSolution(int[] arr){
+        List<Integer> ans=new ArrayList<>();
+        int n=arr.length;
+        int max=arr[n-1];
+        ans.add(arr[n-1]);
+        for(int i=n-2;i>=0;i--){
+            if(arr[i]>max){
+                ans.add(arr[i]);
+                max=arr[i];
+            }
+
+        }
+        Collections.reverse(ans);
+        return ans;
+
+    }
+    public static void main(String[] args) {
+        int[] arr = {4, 7, 1, 0};
+        List<Integer> lead=bruteforce(arr);
+        System.out.println(lead);
+        int[] nums={10,22,12,3,0,6};
+        System.out.println(optimalSolution(nums));
+    }
+
+}
---
+++ java_solutions/Array/LongestContinuousSequence.java
@@ -0,0 +1,86 @@
+package java_solutions.Array;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+
+public class LongestContinuousSequence {
+    public static boolean LinearSearch(int[] arr,int num){
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            if(arr[i]==num)return true;
+            
+        }
+        return false;
+    }
+    public static int bruteforce(int[] arr ){
+        
+        
+        int n=arr.length;
+        int longest=1;
+        if(n==0)return 0;
+        for(int i=0;i<n;i++){
+            int x=arr[i];
+            int cnt=1;
+            while(LinearSearch(arr, x+1)==true){
+                x+=1;
+                cnt+=1;
+            }
+            longest=Math.max(longest, cnt);
+        }
+        return longest;
+    }
+    public static int bettersolution(int[] arr){
+        Arrays.sort(arr);
+        int n=arr.length;
+        int count=0;
+        int leastSmaller=Integer.MIN_VALUE;
+        int longest=1;
+        for(int i=0;i<n;i++){
+            if(arr[i]-1==leastSmaller){
+                count+=1;
+                leastSmaller=arr[i];
+
+            }
+            else if(arr[i]!=leastSmaller){
+                count=1;
+                leastSmaller=arr[i];
+            }
+            longest=Math.max(longest,count);
+        }
+        return longest;
+
+    }
+
+    public static int optimalsolution(int[] arr){
+        HashSet<Integer> hash=new HashSet<>();
+        int n=arr.length;
+        if(n==0)return 0;
+        for(int i=0;i<n;i++){
+            hash.add(arr[i]);
+
+        }
+        int longest=1;
+        for(int items:hash){
+            if(!hash.contains(items-1)){
+                int cnt=1;
+                int x=items;
+                while(hash.contains(x+1)){
+                    x++;
+                    cnt++;
+                }
+                longest=Math.max(cnt, longest);
+            }
+
+        }
+        return longest;
+
+    }
+    public static void main(String[] args) {
+        int[] nums = {100, 4, 200, 1, 3, 2};
+        System.out.println(bruteforce(nums));
+        System.out.println(bettersolution(nums));        
+        System.out.println(optimalsolution(nums));        
+        
+    }
+}
